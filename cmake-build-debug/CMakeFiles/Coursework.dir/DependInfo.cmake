392c3bd2856a8002ca978a0e2565c05300805dd4
lilarin <80650911+lilarin@users.noreply.github.com>
2022-06-10 15:37:19 +0300

    Update

--- cmake-build-debug/CMakeFiles/Coursework.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Coursework.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/User/Desktop/Coursework/Coursework/main.cpp" "C:/Users/User/Desktop/Coursework/Coursework/cmake-build-debug/CMakeFiles/Coursework.dir/main.cpp.obj"
+  "C:/Users/User/Desktop/Coursework/main.cpp" "C:/Users/User/Desktop/Coursework/cmake-build-debug/CMakeFiles/Coursework.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     string equation,cursorPosition;
     float width = 795, height = 720, renderWidth = 1280, renderHeight = 720,
     xOriginal = 0, yOriginal = 0, CenterX = (width/2), CenterY = (height/2),Scaler = 45, PanX, PanY;
-    bool isLeftHold = false, isRightHold = false, isGridVisible = true, isThemeBlack = true, isCursorVisible = false;
+    bool isRightHold = false, isGridVisible = true, isThemeBlack = true, isCursorVisible = false;
 
     // rendering window and apply framerate
     RenderWindow window;
