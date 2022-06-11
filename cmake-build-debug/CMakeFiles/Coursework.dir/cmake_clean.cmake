2347024cd10658e8b01f8637a9ca06bc7233cbd4
lilarin <80650911+lilarin@users.noreply.github.com>
2022-06-11 22:57:58 +0300

    Update

---
+++ cmake-build-debug/CMakeFiles/Coursework.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coursework.dir/main.cpp.obj"
+  "Coursework.exe"
+  "Coursework.exe.manifest"
+  "Coursework.pdb"
+  "libCoursework.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coursework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
